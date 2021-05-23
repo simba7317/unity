b6573f1b6547065ea85d7d3c4f70f5fec1c63b85
simba7317 <simba7317@daum.net>
2021-05-23 21:51:48 +0900

    team project

---
+++ JellyController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JellyController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, speed * Time.deltaTime, 0);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D Col)
+    {
+        if (Col.gameObject.tag == "SHARK")
+        {
+            SceneManager.LoadScene("GameOverScene");
+        }
+    }
+
+}
